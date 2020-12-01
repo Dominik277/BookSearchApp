2180fd17b6a2de5aa50e213a055392b2081a2303
Dominik277 <dominikdoric277@gmail.com>
2020-12-01 18:33:25 +0100

    Dodana BookAdapter klasa koja nam omogucuje prikazivnja klase Book i njenih objekata u određeni view.

---
+++ app/src/main/java/book/search/app/BookAdapter.java
@@ -0,0 +1,27 @@
+package book.search.app;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class BookAdapter extends ArrayAdapter<Book> {
+
+    //Book adapter nam omogućava da objekt od klase Book pretvorimo u nekakav View
+
+    //Konstruktor
+    public BookAdapter(Context context, ArrayList<Book> aBooks){
+        super(context,0,aBooks);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return null;
+    }
+}
