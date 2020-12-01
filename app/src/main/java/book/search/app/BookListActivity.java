929580f69a2035a977d829e44b6a57bc3ecd6a8f
Dominik277 <dominikdoric277@gmail.com>
2020-12-01 20:53:31 +0100

    Metoda fetchBooks sluzi kako bi poslali poziv za book search API.

--- app/src/main/java/book/search/app/BookListActivity.java
+++ app/src/main/java/book/search/app/BookListActivity.java
@@ -1,9 +1,15 @@
 package book.search.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Headers;
 
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -11,6 +17,7 @@ public class BookListActivity extends AppCompatActivity {
 
     private ListView lvBooks;
     private BookAdapter bookAdapter;
+    private BookClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,35 @@ public class BookListActivity extends AppCompatActivity {
         ArrayList<Book> aBooks = new ArrayList<Book>();
         bookAdapter = new BookAdapter(this,aBooks);
         lvBooks.setAdapter(bookAdapter);
+    }
+
+    private void fetchBooks(){
+        client = new BookClient();
+        client.getBooks("oscar Wilde", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON response) {
+                try {
+                    JSONArray docs = null;
+                    if (response != null){
+                        docs = response.getJSONArray("docs");
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        bookAdapter.clear();
+                        for (Book book : books){
+                            bookAdapter.add(book);
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+
+
+        });
     }
 }
