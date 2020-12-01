5ac254488a6530ec95031b5160f98f8a6e0d62dc
Dominik277 <dominikdoric277@gmail.com>
2020-12-01 18:40:00 +0100

    Dodan item_book.xml u kojem smo napravili izgled kako ce izgledati određena lista knjige.

--- app/src/main/java/book/search/app/BookAdapter.java
+++ app/src/main/java/book/search/app/BookAdapter.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
 import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
