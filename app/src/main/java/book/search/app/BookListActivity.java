c373a522d57401100e329d367d85fb34328f0763
Dominik277 <dominikdoric277@gmail.com>
2020-12-01 20:37:01 +0100

    U BookListActivity klasi sve prije napisano skupljamo u jednu cjelinu.Prvo koristimo klijenta da posalje API poziv,onda podatke deserijaliziramo u array of Book objekata i onda te sve podatke ubacimo u ListView pomocu BookAdapter klase.

--- app/src/main/java/book/search/app/BookListActivity.java
+++ app/src/main/java/book/search/app/BookListActivity.java
@@ -3,12 +3,23 @@ package book.search.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class BookListActivity extends AppCompatActivity {
+
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+
+        lvBooks = findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<Book>();
+        bookAdapter = new BookAdapter(this,aBooks);
+        lvBooks.setAdapter(bookAdapter);
     }
 }
