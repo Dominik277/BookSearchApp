a3d9624a30301f0d31f7e4eac504a2cdcefce577
Dominik277 <dominikdoric277@gmail.com>
2020-12-02 09:06:04 +0100

    Populacija podataka iz Book u details View s pomocu BookDetailActivity.

--- app/src/main/java/book/search/app/BookDetailActivity.java
+++ app/src/main/java/book/search/app/BookDetailActivity.java
@@ -1,11 +1,23 @@
 package book.search.app;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Headers;
 
 public class BookDetailActivity extends AppCompatActivity {
 
@@ -20,5 +32,65 @@ public class BookDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+        tvPublisher = (TextView) findViewById(R.id.tvPublisher);
+        tvPageCount = (TextView) findViewById(R.id.tvPageCount);
+        // Use the book to populate the data into our views
+        Book book = (Book) getIntent().getSerializableExtra(BookListActivity.BOOK_DETAIL_KEY);
+        loadBook(book);
+    }
+
+    private void loadBook(Book book){
+        this.setTitle(book.getTitle());
+        Picasso.with(this).load(Uri.parse(book.getLargeCoverUrl()))
+                .error(R.drawable.ic_nocover).into(ivBookCover);
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        client = new BookClient();
+        client.getExtraBookDetails(book.getOpenLibraryId(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON response) {
+                try {
+                    if (response.has("publishers")){
+                        final JSONArray publisher = response.getJSONArray("publishers");
+                        final int numPublishers = publisher.length();
+                        final String[] publishers = new String[numPublishers];
+                        for (int i=0; i<numPublishers; i++){
+                            publishers[i] = publisher.getString(i);
+                        }
+                        tvPublisher.setText(TextUtils.join(", ",publishers));
+                    }
+                    if (response.has("nummber_of_pages")){
+                        tvPageCount.setText(Integer.toString(response
+                                .getInt("number_of_pages")) + " pages");
+                    }
+                }catch (JSONException e){
+                        e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_book_detail,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id==R.id.action_search){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
